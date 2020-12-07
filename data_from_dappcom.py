1b2ebea41926b34b46a4f9273aba398dcb0cc671
Rita Tcepeleva <34192171+RitaTcepeleva@users.noreply.github.com>
2020-12-07 17:04:03 +0300

    Update data_from_dappcom.py

--- data_from_dappcom.py
+++ data_from_dappcom.py
@@ -6,32 +6,6 @@ from pycoingecko import CoinGeckoAPI
 
 cg = CoinGeckoAPI()
 
-def get_info(disco_mem):
-    web_req = requests.get('https://www.dapp.com/app/rubic')
-    content = BeautifulSoup(web_req.text, 'html.parser')
-    assets = content.find_all('div', class_='sub-stats-item')
-    users_24 = assets[1].contents[8].text
-    total_uni_us = assets[1].contents[6].text
-    assets = content.find_all('div', class_='balance')
-    tvl = assets[0].contents[2].text.replace('\n                        ', ' ')
-    assets = content.find_all('div', class_='contract')
-    contracts = assets[0].contents[2].text+': '+assets[0].contents[0].text.replace('\xa0', '')
-    ############
-    token = cg.get_coin_by_id(id='rubic', community_data='true')
-    twitters = str(token['community_data']['twitter_followers'])
-    telegram_followers = str(token['community_data']['telegram_channel_user_count'])
-    result = users_24+'\n'+total_uni_us+'\n'+tvl+'\n'+contracts+'\nTwitter followers: '+twitters+'\nTelegram followers: '+telegram_followers+'\nDiscord members: '+disco_mem
-    return result
-
-#print(get_info())
-
-def get_price():
-    response = requests.get('https://api.etherscan.io/api?module=gastracker&action=gasoracle')  # Get-запрос
-    json_data = json.loads(response.text)  # Извлекаем JSON
-    propose_gas_price = str('Gas (' + json_data['result']['ProposeGasPrice'] + ' Gwei)')
-    status_price = emoji.emojize(':high_voltage:'+json_data['result']['FastGasPrice']+' | :turtle:'+json_data['result']['SafeGasPrice'])
-    return propose_gas_price, status_price
-
 def get_liq():
     #PRICE INFO
     rbc_usd = cg.get_price(ids='rubic', vs_currencies='usd')['rubic']['usd']
